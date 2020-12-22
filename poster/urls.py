817fd513de86e33dc1b6db303426dcc09b009340
Gustavo Costa <gustavodepfcosta@gmail.com>
2020-12-22 08:46:24 -0300

    Every function based view upgraded to Class Based Views

--- poster/urls.py
+++ poster/urls.py
@@ -5,67 +5,67 @@ from .views import *
 urlpatterns = [
     path(
         '', 
-        index, 
+        IndexView.as_view(), 
         name='index.html',
         ),
 
     path(
         'registrations/', 
-        registrations, 
+        RegistrationsView.as_view(), 
         name='registrations.html',
         ),
 
     path(
         'registrations/new-professor/',
-        new_professor, 
+        NewProfessorView.as_view(), 
         name='new-professor.html',
         ),
 
     path(
         'registrations/new-student/', 
-        new_student, 
+        NewStudentView.as_view(), 
         name='new-student.html',
         ),
 
     path(
         'registrations/new-guardian/', 
-        new_guardian, 
+        NewGuardianView.as_view(), 
         name='new-guardian.html',
         ),
 
     path(
         'registrations/new-subject/', 
-        new_subject, 
+        NewSubjectView.as_view(), 
         name='new-subject.html',
         ),
 
     path(
         'grades-manager/', 
-        grades_manager, 
+        GradesManagerView.as_view(), 
         name='grades-manager.html',
         ),
         
     path(
         'contact/', 
-        contact, 
+        ContactView.as_view(), 
         name="contact.html",
         ),
 
     path(
         'enrollments/',
-        enrollments,
+        EnrollmentsView.as_view(),
         name='enrollments.html',
     ),
 
     path(
         'subscriptions-menu/',
-        subscriptions_menu,
+        SubscriptionsMenuView.as_view(),
         name='subscriptions-menu.html',
     ),
 
     path(
         'student-guardian-menu/',
-        students_guardians_menu,
+        StudentsGuardiansMenuView.as_view(),
         name='student-guardian-menu.html',
     ),
 ]
--- poster/views.py
+++ poster/views.py
@@ -1,201 +1,65 @@
-from django.contrib.messages.api import error
+from django.contrib.messages.api import error, success
 from poster.forms import *
 from django.shortcuts import render
 from django.contrib import messages
+from django.views.generic import TemplateView, CreateView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
 
-def registrations(request):
-    return render(request, 'registrations.html')
+class RegistrationsView(TemplateView):
+    template_name = 'registrations.html'
 
 
-def subscriptions_menu(request):
-    form = SubscriptionsModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'Done!')
-            form = SubscriptionsModelForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong.')
-
-    else:
-        form = SubscriptionsModelForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'subscriptions-menu.html', context)
+class SubscriptionsMenuView(CreateView):
+    template_name = 'subscriptions-menu.html'
+    model = Subscriptions
+    form_class = SubscriptionsModelForm
 
 
-def students_guardians_menu(request):
-    form = StudentGuardianForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'Done!')
-            form = StudentGuardianForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong.')
-
-    else:
-        form = StudentGuardianForm()
-
-    context = {
-        'form': form,
-    }
-            
-    return render(request, 'student-guardian-menu.html', context)
+class StudentsGuardiansMenuView(CreateView):
+    template_name = 'student-guardian-menu.html'
+    model = GuardianStudentBridge
+    form_class = StudentGuardianForm
 
 
-def enrollments(request):
-    return render(request, 'enrollments.html')
+class EnrollmentsView(TemplateView):
+    template_name = 'enrollments.html'
 
 
-def new_student(request):
-    form = StudentModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'New student registered successfully')
-            form = StudentModelForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong. Please, try again later.')
-
-    else:
-        form = StudentModelForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'new-student.html', context)
+class NewStudentView(CreateView):
+    template_name = 'new-student.html'
+    model = Student
+    form_class = StudentModelForm
 
 
-def new_guardian(request):
-    form = GuardianModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'New guardian registered successfully')
-            form = GuardianModelForm
-
-        else:
-            messages.error(request, 'Oops! Something went wrong. Please, try again later.')
-        
-    else:
-        form = GuardianModelForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'new-guardian.html', context)
+class NewGuardianView(CreateView):
+    template_name = 'new-guardian.html'
+    model = Guardian
+    form_class = GuardianModelForm
 
 
-def new_subject(request):
-    form = SubjectModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'New Subject registered successfully')
-            form = SubjectModelForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong. Please, try again later.')
-
-    else:
-        form = SubjectModelForm()
-
-    context = {
-        'form': form
-    }
-
-    return render(request, 'new-subject.html', context)
+class NewSubjectView(CreateView):
+    template_name = 'new-subject.html'
+    model = Subject
+    form_class = SubjectModelForm
  
 
-def grades_manager(request):
-    form = GradesForm()
-
-    if str(request.method) == 'POST':   
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'Grades assigned successfully')
-            form = GradesForm()
-
-        else:
-            messages.error(request, 'Oops! Somethig went worng. Please, try again later!')
-
-    else:
-        form = GradesForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'grades-manager.html', context)
+class NewProfessorView(CreateView):
+    template_name = 'new-professor.html'
+    model = Professor
+    form_class = ProfessorModelForm
 
 
-def new_professor(request):
-    form = ProfessorModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-
-            messages.success(request, 'New professor registered successfully!')
-            form = ProfessorModelForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong. Please, try again later!')
-            
-    else:
-        form = ProfessorModelForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'new-professor.html', context)
+class GradesManagerView(CreateView):
+    template_name = 'grades-manager.html'
+    model = Grades
+    form_class = GradesForm
 
 
-def contact(request):
-    form = ContactModelForm(request.POST or None)
-
-    if str(request.method) == 'POST':
-        if form.is_valid():
-            form.save()
-            form.send_mail()
-
-            messages.success(request, 'Message sent successfully')
-            form = ContactModelForm()
-
-        else:
-            messages.error(request, 'Oops! Something went wrong. Please, try again later.')
-            
-    else:
-        form = ContactModelForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'contact.html', context)
+class ContactView(CreateView):
+    template_name = 'contact.html'
+    model = Contact
+    form_class = ContactModelForm

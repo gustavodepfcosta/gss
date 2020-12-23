7b168d9b348037d9bc2c075c0dd05d1dc876b71c
Gustavo Costa <gustavodepfcosta@gmail.com>
2020-12-23 11:25:29 -0300

    Problem with the success-URL solved. Forms are now being stored on pgsql database again

--- poster/views.py
+++ poster/views.py
@@ -1,4 +1,5 @@
 from django.contrib.messages.api import error, success
+from django.views.generic.edit import FormView
 from poster.forms import *
 from django.shortcuts import render
 from django.contrib import messages
@@ -8,58 +9,77 @@ from django.views.generic import TemplateView, CreateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-
 class RegistrationsView(TemplateView):
     template_name = 'registrations.html'
 
-
-class SubscriptionsMenuView(CreateView):
+class SubscriptionsMenuView(FormView):
     template_name = 'subscriptions-menu.html'
     model = Subscriptions
     form_class = SubscriptionsModelForm
+    success_url = '/subscriptions-menu/'
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class StudentsGuardiansMenuView(CreateView):
+class StudentsGuardiansMenuView(FormView):
     template_name = 'student-guardian-menu.html'
     model = GuardianStudentBridge
     form_class = StudentGuardianForm
 
-
 class EnrollmentsView(TemplateView):
     template_name = 'enrollments.html'
 
-
-class NewStudentView(CreateView):
+class NewStudentView(FormView):
     template_name = 'new-student.html'
     model = Student
     form_class = StudentModelForm
+    success_url = '/registrations/new-student/'
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class NewGuardianView(CreateView):
+class NewGuardianView(FormView):
     template_name = 'new-guardian.html'
     model = Guardian
     form_class = GuardianModelForm
+    success_url = '/registrations/new-guardian/'
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class NewSubjectView(CreateView):
+class NewSubjectView(FormView):
     template_name = 'new-subject.html'
     model = Subject
     form_class = SubjectModelForm
- 
+    success_url = '/registrations/new-subject/'
 
-class NewProfessorView(CreateView):
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class NewProfessorView(FormView):
     template_name = 'new-professor.html'
     model = Professor
     form_class = ProfessorModelForm
+    success_url = '/registrations/new-professor/'
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class GradesManagerView(CreateView):
+class GradesManagerView(FormView):
     template_name = 'grades-manager.html'
     model = Grades
     form_class = GradesForm
+    success_url = '/grades-manager/'
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class ContactView(CreateView):
+class ContactView(FormView):
     template_name = 'contact.html'
     model = Contact
     form_class = ContactModelForm
+    success_url = '/contact/'
+
+    def form_valid(self, form):
+        # form.send_mail()
+        return super().form_valid(form)
